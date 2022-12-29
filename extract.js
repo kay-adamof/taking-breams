d165efe9f0a53c06082d40d7fd1a09db75c53118
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2022-12-29 09:37:43 +0900

    🎨

--- extract.js
+++ extract.js
@@ -1,25 +1,25 @@
-(function(console){
+(function(console) {
 
-    console.save = function(data, filename){
+    console.save = function(data, filename) {
 
-        if(!data) {
+        if (!data) {
             console.error('Console.save: No data')
             return;
         }
 
-        if(!filename) filename = 'console.json'
+        if (!filename) filename = 'console.json'
 
-        if(typeof data === "object"){
+        if (typeof data === "object") {
             data = JSON.stringify(data, undefined, 4)
         }
 
-        var blob = new Blob([data], {type: 'text/json'}),
-            e    = document.createEvent('MouseEvents'),
-            a    = document.createElement('a')
+        var blob = new Blob([data], { type: 'text/json' }),
+            e = document.createEvent('MouseEvents'),
+            a = document.createElement('a')
 
         a.download = filename
         a.href = window.URL.createObjectURL(blob)
-        a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
+        a.dataset.downloadurl = ['text/json', a.download, a.href].join(':')
         e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
         a.dispatchEvent(e)
     }
@@ -27,21 +27,21 @@
 
 
 function extractTextFromXPath(xpath) {
-  // Evaluate the XPath expression
-  let nodes = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
+    // Evaluate the XPath expression
+    let nodes = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
 
-  // Initialize an empty array to store the text
-  let text = [];
+    // Initialize an empty array to store the text
+    let text = [];
 
-  // Iterate over the nodes returned by the XPath expression
-  let node;
-  while (node = nodes.iterateNext()) {
-    // Extract the text from each node and add it to the array
-    text.push(node.textContent);
-  }
+    // Iterate over the nodes returned by the XPath expression
+    let node;
+    while (node = nodes.iterateNext()) {
+        // Extract the text from each node and add it to the array
+        text.push(node.textContent);
+    }
 
-  // Return the array of text
-  return text;
+    // Return the array of text
+    return text;
 }
 
 // Extract the text from the XPath
