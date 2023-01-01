cecfbee34b11da8c488975dd435067de4733a7c6
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-01-01 15:57:41 +0900

    ♻️a with ChatGPT

--- extract.js
+++ extract.js
@@ -1,82 +1,72 @@
-(function(console) {
-
-    console.save = function(data, filename) {
-
-        if (!data) {
-            console.error('Console.save: No data')
-            return;
-        }
-
-        if (!filename) filename = 'console.json'
-
-        if (typeof data === "object") {
-            data = JSON.stringify(data, undefined, 4)
-        }
-
-        var blob = new Blob([data], { type: 'text/json' }),
-            e = document.createEvent('MouseEvents'),
-            a = document.createElement('a')
-
-        a.download = filename
-        a.href = window.URL.createObjectURL(blob)
-        a.dataset.downloadurl = ['text/json', a.download, a.href].join(':')
-        e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
-        a.dispatchEvent(e)
-    }
-})(console)
-
-
 function extractTextFromXPath(xpath) {
-    // Evaluate the XPath expression
-    let nodes = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
+  // Evaluate the XPath expression
+  let nodes = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
 
-    // Initialize an empty array to store the text
-    let text = [];
+  // Initialize an empty array to store the text
+  let text = [];
 
-    // Iterate over the nodes returned by the XPath expression
-    let node;
-    while (node = nodes.iterateNext()) {
-        // Extract the text from each node and add it to the array
-        text.push(node.textContent);
-    }
+  // Iterate over the nodes returned by the XPath expression
+  let node;
+  while (node = nodes.iterateNext()) {
+    // Extract the text from each node and add it to the array
+    text.push(node.textContent);
+  }
 
-    // Return the array of text
-    return text;
+  // Return the array of text
+  return text;
 }
 
 function makeXPaths(xpath, n) {
-    let XPaths = [];
-    for (let i = 1; i <= n; i++) {
-        let updatedXPath = xpath.replace(xpath, `/html/body/div[2]/div[5]/div[3]/div/div[1]/div[5]/div[${i}]/article/div[2]/h2/a/span`);
-        XPaths.push(updatedXPath)
-    }
-    return XPaths
+  let XPaths = [];
+  for (let i = 1; i <= n; i++) {
+    let updatedXPath = xpath.replace(xpath, `/html/body/div[2]/div[5]/div[3]/div/div[1]/div[5]/div[${i}]/article/div[2]/h2/a/span`);
+    XPaths.push(updatedXPath);
+  }
+  return XPaths;
 }
 
 function makeTexts(XPaths) {
-    let Texts = []
-    XPaths.forEach(xpath => {
-        Texts.push(extractTextFromXPath(xpath))
-    });
-    return Texts
+  let texts = [];
+  XPaths.forEach((xpath) => {
+    texts.push(extractTextFromXPath(xpath));
+  });
+  return texts;
 }
+
 // Extract the text from the XPath
-const XPath = '/html/body/div[2]/div[5]/div[3]/div/div[1]/div[5]/div[2]/article/div[2]/h2/a/span'
-const articleQuantity = 10
-let XPaths = makeXPaths(XPath, articleQuantity)
-let Texts =[]
-XPaths.forEach(xpath => {
-    console.log(extractTextFromXPath(xpath))
-    Texts.push(extractTextFromXPath(xpath))
-});
-console.log(Texts)
+const XPath = '/html/body/div[2]/div[5]/div[3]/div/div[1]/div[5]/div[2]/article/div[2]/h2/a/span';
+const articleQuantity = 10;
+const XPaths = makeXPaths(XPath, articleQuantity);
+const texts = makeTexts(XPaths);
 
-const jsonString = JSON.stringify(Texts);
-console.log(jsonString)
+// Convert the texts array to a JSON string
+const jsonString = JSON.stringify(texts);
 
-console.save(jsonString)
-// Print the extracted text
-// console.log(text);
+// Save the JSON string to a file
+(function(console) {
+  console.save = function(data, filename) {
+    if (!data) {
+      console.error('Console.save: No data');
+      return;
+    }
 
-// console.save(text)
+    if (!filename) filename = 'console.json';
+
+    if (typeof data === 'object') {
+      data = JSON.stringify(data, undefined, 4);
+    }
+
+    const blob = new Blob([data], { type: 'text/json' });
+    const e = document.createEvent('MouseEvents');
+    const a = document.createElement('a');
+
+    a.download = filename;
+    a.href = window.URL.createObjectURL(blob);
+    a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
+    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    a.dispatchEvent(e);
+  };
+})(console);
+
+console.save(jsonString);
 
